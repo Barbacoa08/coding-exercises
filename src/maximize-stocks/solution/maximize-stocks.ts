1d23bce527cea40b41f88f3e98e42b27a80b7a90
barbajoe <sabe12070@gmail.com>
2024-12-06 11:26:54 -0600

    add solution

--- src/maximize-stocks/solution/maximize-stocks.ts
+++ src/maximize-stocks/solution/maximize-stocks.ts
@@ -1,3 +1,16 @@
 export const MaximizeStocks = (prices: number[]) => {
-	throw new Error("Not implemented");
+	let gap = 0;
+
+	for (let i = 0; i < prices.length; i++) {
+		const uut = prices[i];
+
+		for (let j = i; j < prices.length; j++) {
+			const possibleGap = prices[j] - uut;
+			if (possibleGap > gap) {
+				gap = possibleGap;
+			}
+		}
+	}
+
+	return gap;
 };
