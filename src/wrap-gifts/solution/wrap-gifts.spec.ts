2162b89688ca4c721933f0108a40fbcdb5a8ae03
barbajoe <sabe12070@gmail.com>
2024-12-12 12:00:04 -0600

    add base tests
    as are described in the requirements

--- src/wrap-gifts/solution/wrap-gifts.spec.ts
+++ src/wrap-gifts/solution/wrap-gifts.spec.ts
@@ -7,4 +7,14 @@ describe("Wrap Gifts", () => {
 		const result = wrapGifts([], Number.POSITIVE_INFINITY);
 		expect(result).toEqual(0);
 	});
+
+	it("should multiple correct answers", () => {
+		expect(wrapGifts([2, 3, 4, 5], 7)).toEqual(2);
+
+		expect(wrapGifts([1, 1, 1, 1, 1, 1, 1], 3)).toEqual(3);
+	});
+
+	it("should a single correct answers", () => {
+		expect(wrapGifts([1, 2, 3, 4, 5], 6)).toEqual(3);
+	});
 });
