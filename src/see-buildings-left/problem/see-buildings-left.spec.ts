b655416d56b43b44209afb1aad2bee637a202ec8
barbajoe <sabe12070@gmail.com>
2024-12-18 11:05:58 -0600

    fix typo

--- src/see-buildings-left/problem/see-buildings-left.spec.ts
+++ src/see-buildings-left/problem/see-buildings-left.spec.ts
@@ -3,7 +3,7 @@ import { expect, describe, it } from "vitest";
 import { seeBuildingsLeft } from "./see-buildings-left";
 
 describe("See Buildings Left", () => {
-	it("should handle no meals", () => {
+	it("should handle no input", () => {
 		const result = seeBuildingsLeft([]);
 		expect(result).toEqual(0);
 	});
--- src/see-buildings-left/solution/see-buildings-left.spec.ts
+++ src/see-buildings-left/solution/see-buildings-left.spec.ts
@@ -3,8 +3,7 @@ import { expect, describe, it } from "vitest";
 import { seeBuildingsLeft } from "./see-buildings-left";
 
 describe("See Buildings Left", () => {
-	it("should handle no meals", () => {
-		const result = seeBuildingsLeft([]);
-		expect(result).toEqual(0);
+	it("should handle no input", () => {
+		expect(seeBuildingsLeft([])).toEqual(0);
 	});
 });
