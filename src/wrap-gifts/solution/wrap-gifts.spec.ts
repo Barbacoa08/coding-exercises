e8614a38e6bda577d9aef56bbb570f9b7ee27498
barbajoe <sabe12070@gmail.com>
2024-12-18 10:43:48 -0600

    add solution

--- src/wrap-gifts/solution/wrap-gifts.spec.ts
+++ src/wrap-gifts/solution/wrap-gifts.spec.ts
@@ -4,17 +4,21 @@ import { wrapGifts } from "./wrap-gifts";
 
 describe("Wrap Gifts", () => {
 	it("should handle no input", () => {
-		const result = wrapGifts([], Number.POSITIVE_INFINITY);
-		expect(result).toEqual(0);
+		expect(wrapGifts([], Number.POSITIVE_INFINITY)).toEqual(0);
+		expect(wrapGifts([1, 2, 4], 0)).toEqual(0);
 	});
 
-	it("should multiple correct answers", () => {
+	it("should handle multiple correct answers", () => {
 		expect(wrapGifts([2, 3, 4, 5], 7)).toEqual(2);
 
 		expect(wrapGifts([1, 1, 1, 1, 1, 1, 1], 3)).toEqual(3);
 	});
 
-	it("should a single correct answers", () => {
+	it("should handle a single correct answers", () => {
 		expect(wrapGifts([1, 2, 3, 4, 5], 6)).toEqual(3);
+	});
+
+	it("should handle unsorted array", () => {
+		expect(wrapGifts([3, 2, 1, 4, 5], 6)).toEqual(3);
 	});
 });
--- src/wrap-gifts/solution/wrap-gifts.ts
+++ src/wrap-gifts/solution/wrap-gifts.ts
@@ -1,3 +1,26 @@
-export const wrapGifts = (giftLengths: number[], paperLength: number) => {
-	throw new Error("Not implemented");
+/**
+ * OPTIMIZATIONS
+ *
+ * assume unsorted array, DO NOT SORT, then solve
+ */
+
+export const wrapGifts = (giftLengths: number[], paperWidth: number) => {
+	if (giftLengths.length === 0 || paperWidth <= 0) return 0;
+
+	const sortedGiftLengths = giftLengths.toSorted();
+
+	let usedPaper = 0;
+	let wrappedGifts = 0;
+	for (let i = 0; i < sortedGiftLengths.length; i++) {
+		const giftLength = sortedGiftLengths[i];
+
+		if (giftLength + usedPaper <= paperWidth) {
+			wrappedGifts++;
+			usedPaper += giftLength;
+		} else {
+			break;
+		}
+	}
+
+	return wrappedGifts;
 };
