00deb26b904a53d3b98a0c3591aa0fb8e97846a4
barbajoe <sabe12070@gmail.com>
2024-12-18 11:08:30 -0600

    add more tests

--- src/see-buildings-left/solution/see-buildings-left.spec.ts
+++ src/see-buildings-left/solution/see-buildings-left.spec.ts
@@ -6,4 +6,24 @@ describe("See Buildings Left", () => {
 	it("should handle no input", () => {
 		expect(seeBuildingsLeft([])).toEqual(0);
 	});
+
+	it("should multiple buildings that only increase in height", () => {
+		expect(seeBuildingsLeft([1, 2, 3, 4, 5])).toEqual(5);
+	});
+
+	it("should multiple buildings that only decrease in height", () => {
+		expect(seeBuildingsLeft([5, 4, 3, 2, 1])).toEqual(1);
+	});
+
+	it("should multiple buildings that vary in height", () => {
+		expect(seeBuildingsLeft([3, 7, 8, 3, 6, 1])).toEqual(3);
+	});
+
+	it("should multiple buildings that vary in height but have a single very tall building at the beginning", () => {
+		expect(seeBuildingsLeft([20, 3, 7, 8, 3, 6, 1])).toEqual(1);
+	});
+
+	it("should multiple buildings that vary in height but the first and last buildings are the tallest", () => {
+		expect(seeBuildingsLeft([20, 3, 7, 8, 3, 6, 30])).toEqual(2);
+	});
 });
