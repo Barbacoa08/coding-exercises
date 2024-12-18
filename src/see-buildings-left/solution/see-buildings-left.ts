2e2312fb151c110985bfad766c1e1ab1cb7069cb
barbajoe <sabe12070@gmail.com>
2024-12-18 11:13:37 -0600

    add solution

--- src/see-buildings-left/solution/see-buildings-left.ts
+++ src/see-buildings-left/solution/see-buildings-left.ts
@@ -1,3 +1,17 @@
 export const seeBuildingsLeft = (heights: number[]) => {
-	throw new Error("Not implemented");
+	if (heights.length <= 0) return 0;
+	if (heights.length === 1) return 1;
+
+	let seenBuildings = 1;
+	let tallestThusFar = heights[0];
+
+	for (let i = 1; i < heights.length; i++) {
+		const h = heights[i];
+		if (tallestThusFar < h) {
+			seenBuildings++;
+			tallestThusFar = h;
+		}
+	}
+
+	return seenBuildings;
 };
