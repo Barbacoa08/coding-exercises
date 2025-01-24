b8410782e8fa8bc20e1c13fb0e07d5407e1f76c9
barbajoe <sabe12070@gmail.com>
2025-01-23 20:44:15 -0600

    complete

--- src/longest-subsequence/solution/longest-subsequence.spec.ts
+++ src/longest-subsequence/solution/longest-subsequence.spec.ts
@@ -4,7 +4,42 @@ import { longestSubsequence } from "./longest-subsequence";
 
 describe("Finds longest sub-sequence of 1 or -1", () => {
 	it("should handle no input", () => {
-		const result = longestSubsequence([]);
-		expect(result).toEqual(0);
+		expect(longestSubsequence([])).toEqual(0);
+	});
+
+	it("should handle input without a sequence", () => {
+		expect(longestSubsequence([3, 7, 1])).toEqual(0);
+		expect(longestSubsequence([3, 1])).toEqual(0);
+		expect(longestSubsequence([1])).toEqual(0);
+	});
+
+	it("should handle when the whole array is consecutive", () => {
+		expect(longestSubsequence([1, 2, 3, 4, 5])).toEqual(5);
+	});
+
+	it("should handle when there is a single pair at the begining", () => {
+		expect(longestSubsequence([1, 2, 4, 1, 5])).toEqual(2);
+	});
+	it("should handle when there is a single pair in the middle", () => {
+		expect(longestSubsequence([4, 2, 3, 1, 5])).toEqual(2);
+	});
+	it("should handle when there is a single pair at the end", () => {
+		expect(longestSubsequence([4, 2, 5, 1, 2])).toEqual(2);
+	});
+
+	it("should handle when there is a triple in the middle", () => {
+		expect(longestSubsequence([4, 2, 3, 4, 1, 5])).toEqual(3);
+	});
+
+	it("should handle when there is a triple in the middle after a pair", () => {
+		expect(longestSubsequence([1, 2, 4, 2, 3, 4, 1, 5])).toEqual(3);
+	});
+
+	it("should handle when there is a triple in the middle before a pair", () => {
+		expect(longestSubsequence([1, 4, 2, 3, 4, 1, 5, 4])).toEqual(3);
+	});
+
+	it("should handle when there is multiple elements consecutive that are both positive and negative", () => {
+		expect(longestSubsequence([4, 2, 3, 4, 3, 2, 3, 5])).toEqual(6);
 	});
 });
--- src/longest-subsequence/solution/longest-subsequence.ts
+++ src/longest-subsequence/solution/longest-subsequence.ts
@@ -1,3 +1,15 @@
 export const longestSubsequence = (arr: number[]) => {
-	throw new Error("Not implemented");
+	let max = 1;
+	let subsequence = 1;
+
+	for (let i = 1; i < arr.length; i++) {
+		if (Math.abs(arr[i] - arr[i - 1]) === 1) {
+			subsequence++;
+			max = Math.max(max, subsequence);
+		} else {
+			subsequence = 1;
+		}
+	}
+
+	return max > 1 ? max : 0;
 };
