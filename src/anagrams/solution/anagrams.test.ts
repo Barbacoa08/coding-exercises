fa777a1f79e15bfaa10faff5c1c34713c14779e6
barbajoe <sabe12070@gmail.com>
2025-01-28 14:09:44 -0600

    add solution

--- src/anagrams/solution/anagrams.test.ts
+++ src/anagrams/solution/anagrams.test.ts
@@ -8,4 +8,21 @@ describe("Find all anagrams", () => {
 		expect(findAnagrams("asdf", "")).toEqual([]);
 		expect(findAnagrams("", "asdf")).toEqual([]);
 	});
+
+	it("should handle no matches", () => {
+		expect(findAnagrams("fish", "cake")).toEqual([]);
+	});
+
+	it("should handle two matches with jumbled characters", () => {
+		expect(findAnagrams("cbaebabacd", "abc")).toEqual([0, 6]);
+	});
+
+	it("should handle many matches in a row", () => {
+		expect(findAnagrams("abab", "ab")).toEqual([0, 1, 2]);
+	});
+
+	it("is case-insensitive", () => {
+		expect(findAnagrams("cBaebabaCd", "abc")).toEqual([0, 6]);
+		expect(findAnagrams("aBAb", "Ab")).toEqual([0, 1, 2]);
+	});
 });
--- src/anagrams/solution/anagrams.ts
+++ src/anagrams/solution/anagrams.ts
@@ -1,3 +1,20 @@
 export const findAnagrams = (s: string, p: string) => {
-	throw new Error("Not implemented");
+	if (s.length <= 0 || p.length <= 0) return [];
+
+	const pSorted = p.toLowerCase().split("").sort().join("");
+	const pLength = p.length;
+	const result = [];
+
+	for (let i = 0; i <= s.length - pLength; i++) {
+		const substring = s
+			.substring(i, i + pLength)
+			.toLocaleLowerCase()
+			.split("")
+			.sort()
+			.join("");
+
+		if (substring === pSorted) result.push(i);
+	}
+
+	return result;
 };
