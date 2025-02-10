78f9fad10ba036098e5d9da85a2f9950daa62a05
barbajoe <sabe12070@gmail.com>
2025-02-10 16:30:05 -0600

    extend tests

--- src/evaluate-postfix/solution/evaluate-postfix.test.ts
+++ src/evaluate-postfix/solution/evaluate-postfix.test.ts
@@ -7,11 +7,15 @@ describe("Evaluate postfix expressions", () => {
 		expect(evaluatePostfix("")).toEqual(0);
 	});
 
-	it("should handle simple addition", () => {
+	it("should handle simple logic", () => {
 		expect(evaluatePostfix("12+")).toEqual(3);
+		expect(evaluatePostfix("12-")).toEqual(-1);
+		expect(evaluatePostfix("12*")).toEqual(2);
+		expect(evaluatePostfix("21/")).toEqual(2);
 	});
 
 	it("should handle appropriate order of operations", () => {
 		expect(evaluatePostfix("56+7*")).toEqual(77);
+		expect(evaluatePostfix("12+3/")).toEqual(1);
 	});
 });
