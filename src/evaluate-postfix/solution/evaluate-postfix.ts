034d1e209d9dedcf6353fabba9c03c1f265ea38f
barbajoe <sabe12070@gmail.com>
2025-02-10 16:52:29 -0600

    working

--- src/evaluate-postfix/solution/evaluate-postfix.ts
+++ src/evaluate-postfix/solution/evaluate-postfix.ts
@@ -1,3 +1,30 @@
 export const evaluatePostfix = (s: string): number => {
-	throw new Error("Not implemented");
+	if (s.length < 3) return 0;
+
+	const operations: string[] = [];
+	const input = s.split("");
+
+	for (let i = 0; i < input.length; i++) {
+		const element = input[i];
+
+		if (+element) {
+			operations.push(element);
+		} else {
+			const a = operations.shift();
+			const b = operations.shift();
+
+			// biome-ignore lint/security/noGlobalEval: only way to compute
+			const result = eval(`${a} ${element} ${b}`);
+
+			operations.push(result);
+		}
+	}
+
+	if (operations.length !== 1) {
+		throw new Error(
+			`Unexpected operations remaining to be done. Current operations: ${operations.join(", ")}`,
+		);
+	}
+
+	return operations.length === 1 ? Number(operations.pop()) : 0;
 };
