3ae4b1f6573074113795c89a28563ed89ab1b2d5
barbajoe <sabe12070@gmail.com>
2025-02-13 16:52:18 -0600

    add trivial solution

--- src/available-numbers/solution/available-numbers.ts
+++ src/available-numbers/solution/available-numbers.ts
@@ -1,3 +1,42 @@
-export const availableNumbers = (s: string, n: number[]): number[] => {
-	throw new Error("Not implemented");
+export const createArrayRange = (start: number, end: number): number[] =>
+	Array.from(new Array(end - start + 1), (_x, i) => i + start);
+
+export type NFLPositionType =
+	| "QB"
+	| "RB"
+	| "WR"
+	| "TE"
+	| "OL"
+	| "DL"
+	| "LB"
+	| "DB"
+	| "KP"
+	| "LS";
+
+export const NFLPostionNumberRanges: Record<NFLPositionType, number[]> = {
+	QB: createArrayRange(0, 19),
+	RB: [...createArrayRange(0, 49), ...createArrayRange(80, 89)],
+	WR: [...createArrayRange(0, 49), ...createArrayRange(80, 89)],
+	TE: [...createArrayRange(0, 49), ...createArrayRange(80, 89)],
+	OL: createArrayRange(50, 79),
+	DL: [...createArrayRange(50, 79), ...createArrayRange(90, 99)],
+	LB: [...createArrayRange(0, 59), ...createArrayRange(90, 99)],
+	DB: createArrayRange(0, 49),
+	KP: [...createArrayRange(0, 49), ...createArrayRange(90, 99)],
+	LS: createArrayRange(0, 99),
+};
+
+export const availableNumbers = (s: NFLPositionType, n: number[]): number[] => {
+	if (NFLPostionNumberRanges[s].length < 1) {
+		throw new Error(`Invalid position type: ${s}`);
+	}
+
+	const result = [];
+	for (const possibleNumber of NFLPostionNumberRanges[s]) {
+		if (!n.includes(possibleNumber)) {
+			result.push(possibleNumber);
+		}
+	}
+
+	return result;
 };
