deceb9ba6858d2646cd427e69930db429b17714a
barbajoe <sabe12070@gmail.com>
2025-03-26 17:00:28 -0500

    add solution

--- GoLang/longest-streak/solution/longest-streak.go
+++ GoLang/longest-streak/solution/longest-streak.go
@@ -1,5 +1,28 @@
 package streak
 
 func LongestStreak(arr []bool, n int) int {
-	return -1
+	if len(arr) <= int(n) || len(arr) < 1 {
+		return 0
+	}
+
+	uut := arr[0]
+	streak := 1
+	peak := 1
+
+	for i := 1; i < len(arr); i++ {
+		v := arr[i]
+
+		if uut == v {
+			streak++
+			peak = max(peak, streak)
+		} else {
+			streak = 1
+			uut = v
+		}
+	}
+
+	if peak >= n {
+		return peak
+	}
+	return 0
 }
