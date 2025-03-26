587228805bea737386fcb03f260c76acd6032ec7
barbajoe <sabe12070@gmail.com>
2025-03-26 16:34:40 -0500

    improve test cases

--- GoLang/longest-streak/solution/longest-streak_test.go
+++ GoLang/longest-streak/solution/longest-streak_test.go
@@ -1,17 +1,29 @@
 package streak
 
-import "testing"
+import (
+	"testing"
+)
 
-const MaxUint = ^uint(0)
-const MaxInt = int(MaxUint >> 1)
+const MaxInt = int(^uint(0) >> 1)
+
+type TestCases struct {
+	arr  []bool
+	n    int
+	want int
+}
 
 func TestFindLongestStreak(t *testing.T) {
 	t.Run("should handle no values passed", func(t *testing.T) {
-		if result := LongestStreak([]bool{}, MaxInt); result != 0 {
-			t.Errorf("expected 0, got %d", result)
+		cases := []TestCases{
+			{[]bool{}, MaxInt, 0},
+			{[]bool{true, true}, MaxInt, 0},
+			{[]bool{true, false, true}, 2, 0},
 		}
-		if result := LongestStreak([]bool{true, true}, MaxInt); result != 0 {
-			t.Errorf("expected 0, got %d", result)
+		for _, c := range cases {
+			got := LongestStreak(c.arr, c.n)
+			if got != c.want {
+				t.Errorf("expected %d, got %d", c.want, got)
+			}
 		}
 	})
 
