39ba7e8fed9804e0e3a1705e90506bce2b2abb81
barbajoe <sabe12070@gmail.com>
2025-09-22 09:49:40 -0500

    start work on Array Sum problem

---
+++ typescript/src/array-sum/problem/array-sum.ts
@@ -0,0 +1,3 @@
+export const arraySum = (a: number[]) => {
+  throw new Error("Not implemented");
+};
---
+++ typescript/src/array-sum/solution/array-sum.test.ts
@@ -0,0 +1,19 @@
+import { expect, describe, it } from "vitest";
+
+import { arraySum } from "./array-sum";
+
+describe("Array sum", () => {
+  it.todo("handles no input");
+
+  it.todo("handles a single number in the array");
+
+  it.todo("handles 2-4 nummbers in the array");
+
+  it.todo("handles 100 numbers in the array");
+
+  it.todo("handles negative numbers (odd and even array length)");
+
+  it.todo("handles decimals");
+
+  it.todo("gracefully dies when exceeding maximum possible value");
+});
