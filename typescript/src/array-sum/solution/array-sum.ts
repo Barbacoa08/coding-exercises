81d026177f36fe2d2158103ec4d14891fa3db0f9
barbajoe <sabe12070@gmail.com>
2025-09-22 15:20:50 -0500

    code cleanup

--- typescript/src/array-sum/solution/array-sum.ts
+++ typescript/src/array-sum/solution/array-sum.ts
@@ -7,18 +7,16 @@ export const arraySum = (arr: number[]) => {
     return (arr[0] + arr[1]) * 2;
   }
 
-  let sum = arr[0] + arr[1];
+  let result = arr[0] + arr[1];
   for (let i = 1; i < arr.length; i++) {
-    const rightNumberExists = i + 1 < arr.length;
-
     const left = arr[i - 1];
     const middle = arr[i];
-    const right = arr[i + 1];
+    const right = arr[i + 1] || 0;
 
-    const pointSum = rightNumberExists ? left + middle + right : left + middle;
+    const sum = left + middle + right;
 
-    sum += pointSum;
+    result += sum;
   }
 
-  return sum;
+  return result;
 };
